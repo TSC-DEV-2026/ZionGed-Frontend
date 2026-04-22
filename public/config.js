ea737349ea4a416869122f4b36bec67379e3bed0
Ariel15012008 <arielcristiandemenezes4@gmail.com>
2026-04-22 17:10:34 -0300

    fix: ajuste da url

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__APP_CONFIG__ = {
   VITE_API_URL_PROD: "https://apiged.ziondocs.com.br/",
   VITE_API_URL_DEV: "http://localhost:8000/",
-  VITE_API_ENVIRONMENT: "dev",
+  VITE_API_ENVIRONMENT: "prod",
 };
